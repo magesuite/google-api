07752d2990a897a8e87f0c38377f9997ede87a1b
Piotr Matras <piotr.matras@creativestyle.pl>
2025-02-17 09:39:28 +0000

    feat: FC-142 allow using proxy for guzzle request

--- Service/GeoLocationResolver.php
+++ Service/GeoLocationResolver.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MageSuite\GoogleApi\Service;
 
 class GeoLocationResolver
@@ -8,45 +10,27 @@ class GeoLocationResolver
     const GEOLOCATION_URL = 'https://maps.googleapis.com/maps/api/geocode/json';
 
     protected $googleApiParameters = ['key', 'address', 'language', 'region', 'components'];
-
-    /**
-     * @var \GuzzleHttp\Client
-     */
-    protected $http;
-
-    /**
-     * @var \MageSuite\GoogleApi\Helper\Configuration
-     */
-    protected $configuration;
-
-    /**
-     * @var \Psr\Log\LoggerInterface
-     */
-    protected $logger;
+    protected ?\GuzzleHttp\Client $http = null;
 
     public function __construct(
-        \MageSuite\GoogleApi\Helper\Configuration $configuration,
-        \Psr\Log\LoggerInterface $logger
-    )
-    {
-        $this->configuration = $configuration;
-        $this->logger = $logger;
+        protected \MageSuite\GoogleApi\Helper\Configuration $configuration,
+        protected \Psr\Log\LoggerInterface $logger
+    ) {}
 
-        $this->http = new \GuzzleHttp\Client([
-            'timeout' => self::GEOLOCATION_TIMEOUT,
-            'allow_redirects' => true,
-            'http_errors' => false,
-        ]);
-    }
-
-    public function execute($params = [])
+    public function execute(array $params = []): ?object
     {
         $params = $this->prepareParameters($params);
-
-        $response = $this->http->get(self::GEOLOCATION_URL, [
+        $options = [
             'query' => $params,
             'timeout' => self::GEOLOCATION_TIMEOUT
-        ]);
+        ];
+        $httpProxy = $this->configuration->getHttpProxy();
+
+        if (!empty($httpProxy)) {
+            $options['proxy'] = $httpProxy;
+        }
+
+        $response = $this->getClient()->get(self::GEOLOCATION_URL, $options);
 
         if ($response->getStatusCode() != 200){
             $message = sprintf('Problem in GeoLocationResolver request, status code: %s, parameters: %s, response: %s', $response->getStatusCode(), implode(',', $params), $response->getBody()->getContents());
@@ -58,7 +42,7 @@ class GeoLocationResolver
         return json_decode($response->getBody()->getContents());
     }
 
-    public function prepareParameters($params)
+    public function prepareParameters(array $params)
     {
         $params = array_merge($this->configuration->getGoogleApiSettings(), $params);
         $params = array_intersect_key($params, array_flip($this->googleApiParameters));
@@ -66,4 +50,16 @@ class GeoLocationResolver
         return $params;
     }
 
+    protected function getClient(): \GuzzleHttp\Client
+    {
+        if ($this->http === null) {
+            $this->http = new \GuzzleHttp\Client([
+                'timeout' => self::GEOLOCATION_TIMEOUT,
+                'allow_redirects' => true,
+                'http_errors' => false,
+            ]);
+        }
+
+        return $this->http;
+    }
 }
